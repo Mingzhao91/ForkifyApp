25806d5452247d7c7ecb25a476254e925a0e71de
Ming <edwardwhite7@hotmail.com>
2019-08-31 17:26:25 +0100

    search recipes and render them

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,56 @@
-// Global app controller
-import num from './test';
-const x = 3333;
-console.log(`I imported ${num} from another module! Var x is ${x}`)
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader, elementStrings } from './views/base';
+
+/**
+ * Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // 1. Get the query from view
+    const query = searchView.getInput();
+
+    if(query) {
+        // 2. New search object and add to state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+        
+
+        // 4. Search for recipes
+        await state.search.getResults();
+
+        // 5. Render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+
+}
+
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest(`.${elementStrings.paginationBtn}`);
+    
+    if(btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);   
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
+
+
+
+
---
+++ src/js/models/Search.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = 'a895dac900bb2b000f22b473b45ac8f3';
+    
+        try {
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+        } catch(error) {
+            console.log(error);
+        }
+    }
+}
+
