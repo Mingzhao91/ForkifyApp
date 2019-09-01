b225dd37bfdb9942445ffcf02993906d9682ffc4
Ming <edwardwhite7@hotmail.com>
2019-09-01 18:00:33 +0100

    second testing account

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,2 @@
 export const proxy = 'https://cors-anywhere.herokuapp.com/';
-export const key = 'a895dac900bb2b000f22b473b45ac8f3';
+export const key = 'dadd1ba290d9af3df0a6eb3798b7a9ed';//'a895dac900bb2b000f22b473b45ac8f3';
